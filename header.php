1f3a0b5e69c65151f73b2edab8395d97c68764c3
Sakariash <Sakarias@hithub.com>
2021-10-27 03:01:35 +0200

    Updated header.php
    Added a font? I think. Perhaps.

--- header.php
+++ header.php
@@ -5,6 +5,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- <link rel="stylesheet" href="navbar.css"> -->
+    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="/fonts/webfontkit-20211026-170944/stylesheet.css">
+    <link rel="stylesheet" href="/fonts/FuturaLT-Book.ttf">
     <title>Document</title>
 </head>
 
