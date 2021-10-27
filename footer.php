5f1b0276787ffe4c48d08fed2fa13319ba7acbb4
Sakariash <Sakarias@hithub.com>
2021-10-27 03:00:15 +0200

    Updated footer.php
    Icons added

--- footer.php
+++ footer.php
@@ -1,6 +1,11 @@
-</body>
 <footer>
-    &copy; <?php echo date('Y') ?>
+    <div class="icon">
+        <img src="/images/icons8-facebook-f-50.png" alt="facebook icon">
+        <img src="/images/icons8-instagram-50.png" alt="instagram icon">
+    </div>
+    <div class="date">&copy; <?php echo date('Y'); ?> Svensk Vårdresurs | svenskvardresurs@info.se</div>
 </footer>
+</body>
 
 </html>
+</body>
