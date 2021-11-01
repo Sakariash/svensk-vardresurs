e2b7fbc22e67cd048b15403b8e0260bee07c6905
Sakariash <simonsakarias@gmail.com>
2021-11-02 00:31:04 +0100

    Added css file contact.css

--- php/header.php
+++ php/header.php
@@ -10,6 +10,7 @@
     <link rel="stylesheet" href="/css/hero.css">
     <link rel="stylesheet" href="/css/article.css">
     <link rel="stylesheet" href="/css/main.css">
+    <link rel="stylesheet" href="/css/contact.css">
     <link rel="stylesheet" href="/css/review.css">
     <link rel="stylesheet" href="/css/footer.css">
     <link rel="stylesheet" href="/fonts/webfontkit-20211026-170944/stylesheet.css">
