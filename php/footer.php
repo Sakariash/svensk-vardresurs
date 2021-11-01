d283ddcfed9b19fd6d454e071459fd04cdec1159
Sakariash <simonsakarias@gmail.com>
2021-11-02 00:28:05 +0100

    added variables instead of text

---
+++ php/footer.php
@@ -0,0 +1,12 @@
+<footer>
+    <div class="icon">
+        <img src="/images/icons8-facebook-f-50.png" alt="facebook icon">
+        <img src="/images/icons8-instagram-50.png" alt="instagram icon">
+    </div>
+    <div class="date">&copy; <?php echo date('Y');
+                                echo $companyName; ?>|<?php echo $companyMail; ?></div>
+</footer>
+</body>
+
+</html>
+</body>
