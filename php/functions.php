4d7859972d57d4c70b70605308208cec1ab7a78b
Sakariash <simonsakarias@gmail.com>
2021-11-04 13:20:06 +0100

    Update functions.php

--- php/functions.php
+++ php/functions.php
@@ -9,15 +9,15 @@ function availableEmployees(string $city, string $occupation, int $year, string 
     require __DIR__ . '/data.php';
     $available = [];
 
-    foreach ($employees as $value) {
-        if ($value['city'] === $city && $value['occupation'] === $occupation && intval($value['experience']) >= $year && $value['contract'] === $contract) {
+    foreach ($employees as $value) :
+        if ($value['city'] === $city && $value['occupation'] === $occupation && intval($value['experience']) >= $year && $value['contract'] === $contract) :
             array_push($available, $value);
-        }
-    }
+        endif;
+    endforeach;
 
     return $available;
 }
-function getSocialLink($icons): string
+function getSocialLink(string $icons): string
 {
     if ($icons === 'instagram') {
         return "https://instagram.com";
