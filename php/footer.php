0f831626b31f2699d5172d449bf3ca064e8b601d
Sakariash <simonsakarias@gmail.com>
2021-11-04 13:10:37 +0100

    Added function

--- php/footer.php
+++ php/footer.php
@@ -1,7 +1,7 @@
 <footer>
     <div class="icon">
-        <img src="/images/icons8-facebook-f-50.png" alt="facebook icon">
-        <img src="/images/icons8-instagram-50.png" alt="instagram icon">
+        <a href="<?php echo getSocialLink('facebook'); ?>"><img src="/svensk_vardresurs/images/icons8-facebook-f-50.png" alt="facebook icon"></a>
+        <a href="<?php echo getSocialLink('instagram'); ?>"><img src="/svensk_vardresurs/images/icons8-instagram-50.png" alt="instagram icon"></a>
     </div>
     <div class="date">&copy; <?php echo date('Y');
                                 echo $companyName; ?>|<?php echo $companyMail; ?></div>
