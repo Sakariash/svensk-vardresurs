ab9131a0d8ad92e875673ae9f7e8f41f9fdcb180
Sakariash <simonsakarias@gmail.com>
2021-11-04 13:10:51 +0100

    Update header.php

--- php/header.php
+++ php/header.php
@@ -5,16 +5,16 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/css/style.css">
-    <link rel="stylesheet" href="/css/navbar.css">
-    <link rel="stylesheet" href="/css/hero.css">
-    <link rel="stylesheet" href="/css/article.css">
-    <link rel="stylesheet" href="/css/main.css">
-    <link rel="stylesheet" href="/css/contact.css">
-    <link rel="stylesheet" href="/css/review.css">
-    <link rel="stylesheet" href="/css/footer.css">
-    <link rel="stylesheet" href="/fonts/webfontkit-20211026-170944/stylesheet.css">
-    <link rel="stylesheet" href="/fonts/FuturaLT-Book.ttf">
+    <link rel="stylesheet" href="/svensk_vardresurs/css/style.css">
+    <link rel="stylesheet" href="/svensk_vardresurs/css/navbar.css">
+    <link rel="stylesheet" href="/svensk_vardresurs/css/hero.css">
+    <link rel="stylesheet" href="/svensk_vardresurs/css/article.css">
+    <link rel="stylesheet" href="/svensk_vardresurs/css/main.css">
+    <link rel="stylesheet" href="/svensk_vardresurs/css/contact.css">
+    <link rel="stylesheet" href="/svensk_vardresurs/css/review.css">
+    <link rel="stylesheet" href="/svensk_vardresurs/css/footer.css">
+    <link rel="stylesheet" href="/svensk_vardresurs/fonts/webfontkit-20211026-170944/stylesheet.css">
+    <link rel="stylesheet" href="/svensk_vardresurs/fonts/FuturaLT-Book.ttf">
     <title>Svensk Vårdresurs</title>
 </head>
 
